501b4a7cf3ae3f59f206a2bff86012fc486bf7df
Danny Sung <danny@dannysung.com>
2024-08-05 14:44:24 -0700

    Initial Commit

---
+++ BugListExpansion/BugListExpansionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugListExpansionApp.swift
+//  BugListExpansion
+//
+//  Created by Danny Sung on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BugListExpansionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BugListExpansionTests/BugListExpansionTests.swift
@@ -0,0 +1,16 @@
+//
+//  BugListExpansionTests.swift
+//  BugListExpansionTests
+//
+//  Created by Danny Sung on 8/5/24.
+//
+
+import Testing
+
+struct BugListExpansionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
